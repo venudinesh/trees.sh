e49395b91f06b67d7d1277af52d72e7bc021b523
Eren Yeager <77774009+venudinesh@users.noreply.github.com>
2023-06-28 21:25:17 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,5 @@ git clone https://gitlab.com/HowWof/android_vendor_realme_nashc.git -b tm vendor
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
 git clone https://github.com/nashc-dev/android_hardware_mediatek_wlan.git -b lineage-20 hardware/mediatek/wlan
 git clone https://github.com/Evolution-X-Devices/hardware_oplus.git -b tiramisu hardware/oplus
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
 git clone https://github.com/venudinesh/OPlusExtras.git -b cherish packages/apps/OPlusExtras
 echo "Nashc source complete"
