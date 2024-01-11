7bb91b2db3f7483573cba05a7984d015b50217fc
Eren Yeager <77774009+venudinesh@users.noreply.github.com>
2024-01-11 14:06:21 +0530

    14

--- sync.sh
+++ sync.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-git clone https://github.com/venudinesh/c18.git -b spark device/realme/nashc
-git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-20 hardware/oplus
-git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-20 vendor/hardware/oplus
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
-git clone https://github.com/venudinesh/mediatek.git -b main hardware/mediatek
-git clone https://github.com/nashc-dev/android_kernel_realme_mt6785.git -b lineage-20 kernel/realme/mt6785
-git clone https://gitlab.com/HowWof/android_vendor_realme_nashc.git -b tm vendor/realme/nashc
-git clone https://github.com/venudinesh/patches.git -b main patches
-echo "Nashc source complete"
+git clone https://github.com/venudinesh/device_realme_nashc_14.git-b alphadroid device/realme/nashc
+git clone https://github.com/venudinesh/vendor_realme_nashc.git -b rising vendor/realme/nashc
+git clone https://github.com/Fastledx/android_hardware_oplus.git -b lineage-21 hardware/oplus
+git clone https://github.com/PQEnablers-Devices/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/venudinesh/kernel_realme_nashc.git -b 14 kernel/realme/mt6785
+git clone https://github.com/Fastledx/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
+git clone https://github.com/nashc-dev/android_hardware_mediatek_wlan.git -b lineage-20 hardware/mediatek/wlan
